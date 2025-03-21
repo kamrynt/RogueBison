ae25351fb34443f8eeffa13931f12320b245c4d4
kamrynt <kamryn.tate@bison.howard.edu>
2025-03-21 14:18:01 -0400

    trying to merge into main

--- Characters/base/PlayerCharacterBase.gd
+++ Characters/base/PlayerCharacterBase.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 class_name PlayerCharacterBase
 
-const FRICTION: float = 0.15
-
-
 @export var healthNode: HealthComponent
 @export var movementNode: UserMovementComponent
 @export var attackNode: AttackComponent
